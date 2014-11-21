cda9a84a08ae823e0d752bd4b2383cfe7e5535ee
Tom Coleman <tom@thesnail.org>
2014-11-21 20:32:36 +1100

    Refactored all shapes

--- client/templates/geometry.js
+++ client/templates/geometry.js
@@ -1,13 +1,3 @@
 Template.geometry.helpers({
-  r: function() {
-    return SVG_WIDTH;
-  },
-  rectAttributes: {
-    x: -1 * SVG_WIDTH,
-    y: -1 * SVG_HEIGHT,
-    width: 2 * SVG_WIDTH,
-    height: 2 * SVG_HEIGHT
-  },
-  triangleTransform: 'matrix(' + 2*SVG_WIDTH + ', 0, 0, ' + 2*SVG_WIDTH + ', ' 
-    + (-1*SVG_WIDTH) + ', ' + (-1*SVG_WIDTH) + ')'
+  transform: 'scale(' + SVG_WIDTH + ')'
 });
